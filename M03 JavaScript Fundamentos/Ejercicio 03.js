c0af9e158d6b0efc828052367dec9ff3ea11f364
GuerraxP <Yorgeguerra@gmail.com>
2023-09-30 14:29:54 -0400

    ejercicios M03

--- M03 JavaScript Fundamentos/Ejercicio 03.js
+++ M03 JavaScript Fundamentos/Ejercicio 03.js
@@ -5,36 +5,78 @@ function sonIguales(x, y) {
    // Retorna true si "x" e "y" son iguales.
    // De lo contrario, retorna false.
    // Tu código:
+   var ban = Boolean;
+   if (x === y)
+      ban = true
+   else
+      ban = false
+
+   return ban
 }
 
 function tienenMismaLongitud(str1, str2) {
    // Retorna true si los dos strings tienen la misma longitud.
    // De lo contrario, retorna false.
    // Tu código:
+   var ban = Boolean;
+   if (str1.length===str2.length)
+      ban = true
+   else
+      ban = false
+   
+   return ban
 }
 
 function menosQueNoventa(num) {
    // Retorna true si el argumento "num" es menor que noventa.
    // De lo contrario, retorna false.
    // Tu código:
+   var ban = Boolean;
+   if (num<90)
+      ban = true
+   else
+      ban = false
+
+   return ban
 }
 
 function mayorQueCincuenta(num) {
    // Retorna true si el argumento "num" es mayor que cincuenta.
    // De lo contrario, retorna false.
    // Tu código:
+   var ban = Boolean;
+   if (num>50)
+      ban = true
+   else
+      ban = false
+
+   return ban
 }
 
 function esPar(num) {
    // Retorna true si "num" es par.
    // De lo contrario, retorna false.
    // Tu código:
+   var ban = Boolean;
+   if (num%2===0)
+      ban = true
+   else
+      ban = false
+
+   return ban
 }
 
 function esImpar(num) {
    // Retorna true si "num" es impar.
    // De lo contrario, retorna false.
    // Tu código:
+   var ban = Boolean;
+   if (num%2!=0)
+      ban = true
+   else
+      ban = false
+
+   return ban
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
